1b7d0227bf11b056076bed53db69fff4047b2561
vlad <ecscalibur_10@mail.ru>
2024-07-11 18:26:12 +0300

    visualizer

--- services/app.py
+++ services/app.py
@@ -11,10 +11,27 @@ class App:
     def __init__(self):
         self.root = Tk()
         self.create_window()
-        self.toolbar = Frame(self.root, bg='gray', relief=SOLID, borderwidth=1)
-        self.create_toolbar_grid()
-        self.create_filedialog()
-        self.choose_fields = Frame(self.toolbar, bg='gray', relief=SOLID, borderwidth=1)
+
+        self.toolbar = None
+        self.create_toolbar()
+
+        self.open_file_button = ttk.Button(master=self.toolbar, text='Открыть файл', command=self.open_json_file)
+        self.open_file_button.grid(row=0, column=0)
+
+        self.filename_label = ttk.Label(self.toolbar, background='gray')
+        self.filename_label.grid(row=0, column=1)
+
+        self.choose_device_combobox = ttk.Combobox(self.toolbar, state='readonly')
+        self.choose_device_combobox.grid(row=0, column=3)
+
+        self.choose_fields = Frame(self.toolbar, bg='gray', relief=SOLID)
+        self.choose_fields.grid(row=1, column=0, columnspan=2)
+
+        self.selection_label = ttk.Label(master=self.choose_fields, text="Вы выбрали:")
+        self.selection_label.pack(anchor=NW, fill=X, padx=5, pady=5)
+
+        self.fields_listbox = Listbox(master=self.choose_fields, selectmode=MULTIPLE)
+        self.fields_listbox.pack(anchor=NW, fill=X, padx=5, pady=5)
 
     def create_window(self):
         self.root.geometry('500x500+400+150')
@@ -22,17 +39,38 @@ class App:
         self.root.iconbitmap('graph-5_icon.ico')
         self.root.configure(bg='gray')
 
-    def create_toolbar_grid(self):
+    def create_toolbar(self):
+        self.toolbar = Frame(self.root, bg='gray', relief=SOLID, borderwidth=1)
         for c in range(3):
             self.toolbar.columnconfigure(index=c, weight=1)
         for r in range(3):
             self.toolbar.rowconfigure(index=r, weight=1)
         self.toolbar.pack(fill=X, expand=True, padx=5, pady=5, anchor=N)
 
+    def create_choose_device_combobox(self, devices_dict, devices_list, devices_var):
+        def device_selected(event):
+            self.fields_listbox.delete(0, END)
+            for device in devices_dict[devices_var.get()]:
+                self.fields_listbox.insert(END, device)
+
+        self.choose_device_combobox.configure(textvariable=devices_var, values=devices_list)
+        self.choose_device_combobox.bind("<<ComboboxSelected>>", device_selected)
+        self.choose_fields.grid(row=1, column=0)
+
+    def create_fields_listbox(self, devices_dict, devices_var):
+        def field_selected(event):
+            selected_indices = self.fields_listbox.curselection()
+            selected_fields = ",".join([self.fields_listbox.get(i) for i in selected_indices])
+            msg = f"Вы выбрали: {selected_fields}"
+            self.selection_label["text"] = msg
+
+        self.fields_listbox.configure(listvariable=Variable(value=devices_dict[devices_var.get()]))
+        self.fields_listbox.bind("<<ListboxSelect>>", field_selected)
+
     def open_json_file(self):
         filename = askopenfilename(filetypes=[('JSON files', '*.json')])
-        filename_label = ttk.Label(self.toolbar, text=filename, background='gray', relief=SOLID)
-        filename_label.grid(row=0, column=1)
+        self.filename_label.configure(text=filename)
+
         with open(filename, encoding='UTF-8') as f:
             data = json.load(f)
 
@@ -40,35 +78,8 @@ class App:
         devices_list = list(devices_dict.keys())
         devices_var = StringVar(value=devices_list[0])
 
-        choose_device_combobox = ttk.Combobox(self.toolbar, textvariable=devices_var,
-                                              values=devices_list, state='readonly')
-        choose_device_combobox.grid(row=0, column=3)
-
-        def device_selected(event):
-            fields_listbox.delete(0, END)
-            for device in devices_dict[devices_var.get()]:
-                fields_listbox.insert(END, device)
-
-        choose_device_combobox.bind("<<ComboboxSelected>>", device_selected)
-        self.choose_fields.grid(row=1, column=0, columnspan=2)
-
-        selection_label = ttk.Label(master=self.choose_fields, text="Вы выбрали:")
-        selection_label.pack(anchor=NW, fill=X, padx=5, pady=5)
-
-        def field_selected(event):
-            selected_indices = fields_listbox.curselection()
-            selected_langs = ",".join([fields_listbox.get(i) for i in selected_indices])
-            msg = f"Вы выбрали: {selected_langs}"
-            selection_label["text"] = msg
-
-        fields_listbox = Listbox(master=self.choose_fields, listvariable=Variable(value=devices_dict[devices_var.get()]),
-                                 selectmode=MULTIPLE)
-        fields_listbox.pack(anchor=NW, fill=X, padx=5, pady=5)
-        fields_listbox.bind("<<ListboxSelect>>", field_selected)
-
-    def create_filedialog(self):
-        open_button = ttk.Button(master=self.toolbar, text='Открыть файл', command=self.open_json_file)
-        open_button.grid(row=0, column=0)
+        self.create_choose_device_combobox(devices_dict, devices_list, devices_var)
+        self.create_fields_listbox(devices_dict, devices_var)
 
     def run(self):
         self.root.mainloop()
--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-import json
-
-
 # poca = {'system_RSSI': ('Уровень сигнала Wi-Fi', 'децибелл от уровня 1 мВт (дБм)'),
 #         'color_tempCT': ('Цветовая температура света', 'Кельвин (К)'),
 #         'soil_soilT': ('Температура почвы', 'градусов по Цельсию (°C)'),
@@ -26,57 +23,4 @@ import json
 #                         },
 #                         'КЛОП-МНУ': klop,
 #                         }
-#'Свежесть-19B', 'Опорный барометр', 'Паскаль', 'Тест Студии'
 
-# fields = {}
-#
-# with open(f'data.json', encoding='UTF-8') as f:
-#     data = json.load(f)
-#     for key in data:
-#         if data[key]['uName'] not in fields.keys():
-#             fields[data[key]['uName']] = {}
-#
-#         for field in data[key]['data'].keys():
-#             if field not in fields[data[key]['uName']].keys():
-#                 fields[data[key]['uName']][field] = data[key]['data'][field]
-#
-# for field in fields:
-#     print('\n\n', field, sep='', end='')
-#     for f in fields[field]:
-#         print('\n\t', (f, fields[field][f]), sep='', end='')
-
-
-def is_digit(string):
-    if string.isdigit():
-        return True
-    else:
-        try:
-            float(string)
-            return True
-        except ValueError:
-            return False
-
-
-print(is_digit(''))
-
-# i: int = 0
-# for key in data.keys():
-#     print(data[key])
-#     i += 1
-#     if i == 50:
-#         break
-# print('\n\n\n')
-#
-# utils = {}
-# for key in data.keys():
-#     if data[key]['uName'] not in utils_for_monitoring:
-#         continue
-#     if data[key]['uName'] not in utils:
-#         utils[data[key]['uName']] = []
-#     for field in data[key]['data'].keys():
-#         if field not in utils[data[key]['uName']]:
-#             utils[data[key]['uName']].append(field)
-#
-# for key in utils.keys():
-#     print(key, ':', end=' ')
-#     print(utils[key])
